3c67bcd59d38206f7e99318a870028862e5ddc5f
Jose Vazquez <vazqjose@gmail.com>
2021-08-10 21:38:26 +0000

    added comments

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,4 +1,13 @@
+-- prepare a MYSQL testing environment --
+
+-- Create testing database --
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
+-- Create test user --
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+
+-- test user will have full priviledges on test db --
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+
+-- test user will have select priviledges on performance schema  table --
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
