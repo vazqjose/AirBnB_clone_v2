8a7e98ef93d6f0e790afe9f5b461507878ce44a1
Jose Vazquez <vazqjose@gmail.com>
2021-08-10 21:40:22 +0000

    add comments

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,4 +1,12 @@
+-- create mysql server for application --
+
 CREATE DATABASE IF NOT EXISTS hbnb_dev db;
+
+-- create main user --
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- user has full priviledges on main db --
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+
+-- select priviledge for user on performance schema db --
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
