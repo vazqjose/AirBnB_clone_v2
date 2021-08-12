a87e83b49101f41b12b04ca130191812e2652a59
Jose Vazquez <vazqjose@gmail.com>
2021-08-12 00:37:13 +0000

    changes in if logic

--- console.py
+++ console.py
@@ -88,20 +88,24 @@ class HBNBCommand(cmd.Cmd):
 
     def postcmd(self, stop, line):
         """Prints if isatty is false"""
+
         if not sys.__stdin__.isatty():
             print('(hbnb) ', end='')
         return stop
 
     def do_quit(self, command):
         """ Method to exit the HBNB console"""
+
         exit()
 
     def help_quit(self):
         """ Prints the help documentation for quit  """
+
         print("Exits the program with formatting\n")
 
     def do_EOF(self, arg):
         """ Handles EOF to exit program """
+
         print()
         exit()
 
@@ -151,7 +155,11 @@ class HBNBCommand(cmd.Cmd):
                 # print(parameter)
                 keyPair = parameter.split('=')
                 key = keyPair[0]
-                value = keyPair[1]
+
+                if '\"' in keyPair[1]:
+                    value = (keyPair[1])[1:-1]
+                else:
+                    value = keyPair[1]
 
                 stringArgs = "{} {} {} {}".format(
                     new_instance.__class__.__name__,
@@ -159,7 +167,6 @@ class HBNBCommand(cmd.Cmd):
 
                 HBNBCommand.do_update(self, stringArgs)
                 storage.save()
-
 
     def help_create(self):
         """ Help information for the create method """
@@ -169,6 +176,7 @@ class HBNBCommand(cmd.Cmd):
 
     def do_show(self, args):
         """ Method to show an individual object """
+
         new = args.partition(" ")
         c_name = new[0]
         c_id = new[2]
@@ -197,11 +205,13 @@ class HBNBCommand(cmd.Cmd):
 
     def help_show(self):
         """ Help information for the show command """
+
         print("Shows an individual instance of a class")
         print("[Usage]: show <className> <objectId>\n")
 
     def do_destroy(self, args):
         """ Destroys a specified object """
+
         new = args.partition(" ")
         c_name = new[0]
         c_id = new[2]
@@ -230,11 +240,13 @@ class HBNBCommand(cmd.Cmd):
 
     def help_destroy(self):
         """ Help information for the destroy command """
+
         print("Destroys an individual instance of a class")
         print("[Usage]: destroy <className> <objectId>\n")
 
     def do_all(self, args):
         """ Shows all objects, or all objects of a class"""
+
         print_list = []
 
         if args:
@@ -253,11 +265,13 @@ class HBNBCommand(cmd.Cmd):
 
     def help_all(self):
         """ Help information for the all command """
+
         print("Shows all objects, or all of a class")
         print("[Usage]: all <className>\n")
 
     def do_count(self, args):
         """Count current number of class instances"""
+
         count = 0
         for k, v in storage._FileStorage__objects.items():
             if args == k.split('.')[0]:
@@ -265,11 +279,13 @@ class HBNBCommand(cmd.Cmd):
         print(count)
 
     def help_count(self):
-        """ """
+        """ prints usage count """
+
         print("Usage: count <class_name>")
 
     def do_update(self, args):
         """ Updates a certain object with new info """
+
         c_name = c_id = att_name = att_val = kwargs = ''
 
         # isolate cls from id/args, ex: (<cls>, delim, <id/args>)
@@ -353,6 +369,7 @@ class HBNBCommand(cmd.Cmd):
 
     def help_update(self):
         """ Help information for the update class """
+
         print("Updates an object with new information")
         print("Usage: update <className> <id> <attName> <attVal>\n")
 
