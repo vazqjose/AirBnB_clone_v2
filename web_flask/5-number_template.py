f5d3c92cf08ccc32633634bcf09739b1067d0f72
Jose Vazquez <vazqjose@gmail.com>
2021-09-12 03:57:26 +0000

    t5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 '''
-Write a script that starts a Flask web application
+    Script that starts a Flask web application.
+    Web application must be listening on 0.0.0.0, port 5000
 '''
 
 
@@ -11,41 +12,29 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    """ display “Hello HBNB!” """
-
     return "Hello HBNB!"
 
 
 @app.route('/hbnb')
 def hbnb():
-    """display “HBNB” """
-
     return "HBNB"
 
 
 @app.route('/c/<text>')
-def c(text):
-    ''' display “C ” followed by the text variable '''
-
-    text = text.replace("_", " ")
-    return "C {}".format(text)
+def c_test(text):
+    return "C {}".format(text.replace('_', ' '))
 
 
 @app.route('/python')
 @app.route('/python/<text>')
-def py(text='is cool'):
-    '''The default value of text is “is cool” '''
-
-    text = text.replace("_", " ")
-    return "Python " + text
+def python_text(text="is cool"):
+    return "Python {}".format(text.replace('_', ' '))
 
 
 @app.route('/number/<int:n>')
 def number(n):
-    ''' Display only if integer '''
-
     if isinstance(n, int):
-        return "{:d} is a number".format(n)
+        return "{} is a number".format(n)
 
 
 @app.route('/number_template/<int:n>')
@@ -53,9 +42,7 @@ def number_template(n):
     if isinstance(n, int):
         return render_template('/5-number.html', n=n)
 
-
 if __name__ == "__main__":
-    ''' run() method of Flask class runs the application '''
-
+    app.debug = True
     app.url_map.strict_slashes = False
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000)
