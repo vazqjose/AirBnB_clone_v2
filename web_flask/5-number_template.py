d5c3b84894b64541068785094e799f0a8426b49f
vazqjose <vazqjose@gmail.com>
2021-09-12 01:05:49 -0400

    Update 5-number_template.py

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -10,23 +10,23 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', strict_slashes = False)
 def index():
     return "Hello HBNB!"
 
 
-@app.route('/hbnb')
+@app.route('/hbnb', strict_slashes = False)
 def hbnb():
     return "HBNB"
 
 
-@app.route('/c/<text>')
+@app.route('/c/<text>', strict_slashes = False)
 def c_test(text):
     return "C {}".format(text.replace('_', ' '))
 
 
-@app.route('/python')
-@app.route('/python/<text>')
+@app.route('/python', strict_slashes = False)
+@app.route('/python/<text>', strict_slashes = False)
 def python_text(text="is cool"):
     return "Python {}".format(text.replace('_', ' '))
 
