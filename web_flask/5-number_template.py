2137ad395d70e9a2a282c1e69d693145cb419729
Jose Vazquez <vazqjose@gmail.com>
2021-09-12 03:29:44 +0000

    t5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -40,7 +40,7 @@ def number(n):
 @app.route('/number_template/<int:n>')
 def number_template(n):
     if isinstance(n, int):
-        return render_template('/5-number.html', n=n)
+        return render_template('/5-number.html', number=n)
 
 if __name__ == "__main__":
     app.debug = True
