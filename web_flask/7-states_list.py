c0a90adb4dc595453fe6841d6a162c85cbdfeb32
Jose Vazquez <vazqjose@gmail.com>
2021-09-20 02:33:01 +0000

    Update to repo

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -4,15 +4,14 @@
 '''
 
 from flask import Flask, render_template
+from models import *
 from models import storage
-from models.state import State
-
 app = Flask(__name__)
 
 
 @app.route("/states_list", strict_slashes=False)
 def states_list():
-    states = storage.all(State).values()
+    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
     return render_template("7-states_list.html", states=states)
 
 
@@ -21,4 +20,4 @@ def teardown(ext):
     storage.close()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host='0.0.0.0', port='5000')
